8bcf88cd9e5cf8dc8519115cf4ad1c674d21fc26
Robey Pointer <robey@twitter.com>
2010-06-26 15:18:55 -0700

    checkpoint: basically works.

--- src/main/scala/net/lag/dvorky/Dvorky.scala
+++ src/main/scala/net/lag/dvorky/Dvorky.scala
@@ -5,6 +5,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.content.Intent
+import android.text.InputType
+import android.content.Context
+import android.util.AttributeSet
+import android.inputmethodservice.{InputMethodService, Keyboard, KeyboardView}
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+
 
 class Dvorky extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
@@ -15,62 +22,102 @@ class Dvorky extends Activity {
   }
 }
 
-
-import android.inputmethodservice.{InputMethodService, Keyboard, KeyboardView}
-import android.view.inputmethod.EditorInfo
-
 class DvorkyKeyboard extends InputMethodService with KeyboardView.OnKeyboardActionListener {
-  var mainKeyboard: Keyboard = null
+  var keyboard1: Keyboard = null
+  var keyboard2: Keyboard = null
   var view: KeyboardView = null
 
+  // capslock:
+  var lastShiftTime: Long = 0
+  var capsLock = false
+
   override def onInitializeInterface() {
-    mainKeyboard = new Keyboard(this, R.xml.main)
+    keyboard1 = new Keyboard(this, R.xml.keyboard1)
+    keyboard2 = new Keyboard(this, R.xml.keyboard2)
   }
 
   override def onCreateInputView() = {
     view = getLayoutInflater().inflate(R.layout.input, null).asInstanceOf[KeyboardView]
-    view.setKeyboard(mainKeyboard)
+    view.setKeyboard(keyboard1)
     view.setOnKeyboardActionListener(this)
     view
   }
-  
+
   override def onStartInput(attribute: EditorInfo, restarting: Boolean) {
     super.onStartInput(attribute, restarting)
-    println("hello!")
   }
 
   override def onStartInputView(attribute: EditorInfo, restarting: Boolean) {
     super.onStartInputView(attribute, restarting)
-    view.setKeyboard(mainKeyboard)
+    view.setKeyboard(keyboard1)
     view.closing()
-    println("hello 2!")
   }
-  
-  
+
+
   // OnKeyboardActionListener
-  
-  def onKey(primaryCode: Int, keyCodes: Array[Int]) { }
+
+  def onKey(primaryCode: Int, keyCodes: Array[Int]) {
+    println("keycode:" + primaryCode + "/" + keyCodes.toList.toString)
+    primaryCode match {
+      case Keyboard.KEYCODE_DELETE =>
+        sendDownUpKeyEvents(KeyEvent.KEYCODE_DEL)
+      case Keyboard.KEYCODE_SHIFT if view ne null =>
+        if (view.getKeyboard() == keyboard1) {
+          checkToggleCapsLock()
+          view.setShifted(capsLock || !view.isShifted())
+        }
+      case Keyboard.KEYCODE_CANCEL =>
+        requestHideSelf(0)
+        view.closing()
+      case Keyboard.KEYCODE_MODE_CHANGE if view ne null =>
+        if (view.getKeyboard() == keyboard1) {
+          view.setKeyboard(keyboard2)
+        } else {
+          view.setKeyboard(keyboard1)
+        }
+        keyboard1.setShifted(false)
+        keyboard2.setShifted(false)
+      case _ =>
+        handleKey(primaryCode, keyCodes)
+        updateShiftKeyState()
+    }
+  }
+
+  private def handleKey(primaryCode: Int, keyCodes: Array[Int]) {
+    val translatedCode = if (isInputViewShown() && view.isShifted()) {
+      Character.toUpperCase(primaryCode)
+    } else {
+      primaryCode
+    }
+    getCurrentInputConnection().commitText(String.valueOf(translatedCode.toChar), 1)
+  }
+
+  private def checkToggleCapsLock() {
+    val now = System.currentTimeMillis();
+    if (lastShiftTime + 800 > now || capsLock) {
+      capsLock = !capsLock
+      lastShiftTime = 0
+    } else {
+      lastShiftTime = now;
+    }
+  }
+
+  private def updateShiftKeyState() {
+    val ei = getCurrentInputEditorInfo()
+    if ((ei ne null) && (view ne null) && (view.getKeyboard() == keyboard1)) {
+      val caps = (ei.inputType != InputType.TYPE_NULL && getCurrentInputConnection().getCursorCapsMode(ei.inputType) != 0)
+      view.setShifted(capsLock || caps)
+    }
+  }
+
+  def onPress(primaryCode: Int) { }
+  def onRelease(primaryCode: Int) { }
   def onText(text: CharSequence) { }
-  def updateCandidates() { }
-  def setSuggestions(suggestions: JList[String], completions: Boolean) { }
-  def handleBackspace() { }
-  def handleShift() { }
-  def handleCharacter(primaryCode: Int, keyCodes: Array[Int]) { }
-  def handleClose() { }
-  def checkToggleCapsLock() { }
-  def isWordSeparator(code: Int): Boolean = { code == 20 }
-  def pickDefaultCandidate() { }
-  def pickSuggestionManually(index: Int) { }
   def swipeRight() { }
   def swipeLeft() { }
   def swipeDown() { }
   def swipeUp() { }
-  def onPress(primaryCode: Int) { }
-  def onRelease(primaryCode: Int) { }
 }
-
-import android.content.Context
-import android.util.AttributeSet
 
 class DvorkyKeyboardView(context: Context, attrs: AttributeSet) extends KeyboardView(context, attrs) {
   val KEYCODE_OPTIONS = -100;
